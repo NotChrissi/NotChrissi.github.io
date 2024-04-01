921976377fb54aba404c213e99fea8965cffb6a7
PC <christopher.drewes@outlook.com>
2024-04-02 01:06:03 +0200

    adad

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 function change(){
-  console.log("hi");
+  var link = document.getElementById("link")
+  var blockEl = document.getElementById("block")
+  var spieltagEl = document.getElementById("spieltag")
+  var anzahlEl = document.getElementById("anzahl")
+
+  var block = blockEl.value;
+  var spieltag = spieltagEl.value;
+  var anzahl = anzahlEl.value;
+
+  var href = "https://mainaqila.github.io/?";
+
+  if(block > 0){
+    href += "&block=" + block
+  }
+  if(spieltag > 0){
+    href += "&spieltag=" + spieltag
+  }
+  if(anzahl > 0){
+    href += "&anzahl=" + anzahl
+  }
+
+  link.href = href;
+  link.innerHTML = href;
+
 }
