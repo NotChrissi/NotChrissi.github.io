7977950dd0fa136b38e41eeda408db07818a77ca
Laptop <christopher.drewes@outlook.com>
2024-04-01 10:25:42 +0200

    dkasd

--- js/app2.js
+++ js/app2.js
@@ -22,13 +22,13 @@ function slide(n) {
   slideIndex += n;
   if (slideIndex < 0) {
     slideIndex = 0;
-  } else if (slideIndex > 2) {
-    slideIndex = 2;
+  } else if (slideIndex > 3) {
+    slideIndex = 3;
   }
   document.getElementById("slider").style.transform = "translateX(" + (-slideIndex * 25) + "%)";
 
   // Überprüfen, ob der Benutzer versucht, über den sichtbaren Bereich hinaus zu swipen
-  if (slideIndex === 0 && n === -1 || slideIndex === 2 && n === 1) {
+  if (slideIndex === 0 && n === -1 || slideIndex === 3 && n === 1) {
     let interval = setInterval(function() {
       slideIndex -= n; // Ändere die Anzahl der Schritte nach Bedarf
       document.getElementById("slider").style.transform = "translateX(" + (-slideIndex * 25) + "%)";
